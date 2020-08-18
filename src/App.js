64158755fdc8ca27bffb0bcbad02a29ac3ba18cd
k-rohit1999 <rohit.k@gobaskt.com>
2020-08-18 15:04:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {Card, CardTitle, CardBody, Container, Row, Col} from 'reactstrap';
+import Success from './SuccessComponent';
 
 function App() {
 
   useEffect(() => {
     fetchDate();
   }, []);
+
+  const [success, setSuccess] = useState(true);
 
   const [dates, setDate] = useState([]);
   const [times, setTime] = useState([]);
@@ -21,7 +24,6 @@ function App() {
   const bpServiceSubCatogery = urlParams.get('bpServiceSubCatogery');
   const serviceCatogery = urlParams.get('serviceCatogery');
   const serviceName = urlParams.get('serviceName');
-  const userId = urlParams.get('userId');
 
 
   const fetchDate = async () => {
@@ -52,7 +54,7 @@ function App() {
       "userId":"CONS110819219070736"
     }
   };
-  fetch('https://jsonplaceholder.typicode.com/posts', requestOptions)
+  fetch('https://apidev.gobaskt.com/consumerAppointmentsApi/gobaskt/createUserApppointment', requestOptions)
       .then(response => response.json());
 }
 
@@ -67,13 +69,18 @@ function App() {
     setStaffName(name);
   }
 
+  const postAndSuccess=()=>{
+    setSuccess(false);
+    postData();
+  }
+
   const dateSlots = dates.map((date) => {
 
     let dt = new Date(`${date.date}`);
     let d = dt.toDateString().split(' ').slice(1).join(' ');
 
     return(
-              <Col xs="auto" className={`ml-2 mb-4 p-2 border border-secondary rounded`} key={date.date}>
+              <Col xs="auto" className={`ml-2 mb-4 p-1 border border-secondary rounded`} key={date.date}>
                 <button className="btn btn-light" onClick={() => showtime(date.date, date.timeslots)}>{d}</button>
               </Col>
     );
@@ -93,60 +100,71 @@ function App() {
 
   return (
     <>
-      <nav style={{backgroundColor:'#0079ff'}}>
-        <h5 style={{color: '#ffffff'}} className="pt-3 pb-3 ml-2">Schedule Appointment</h5>
-      </nav>
-      <Container>
-          <p style={{fontWeight:'bold', fontSize:'20px'}}>Schedule Appointment at Merchant Name</p>
-          <Row className="justify-content-around">
-            <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
-              <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
-              <p>By Date</p>
-            </Col>
-            <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
-              <img src="/Clock.png" alt="clock icon" style={{width:'20px', height:'20px' }} />
-              <p>By Time</p>
-            </Col>
-            <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
-              <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
-              <p>By Day</p>
-            </Col>
-            <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
-              <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
-              <p>By Month</p>
-            </Col>
-          </Row>
-          <Card className="p-1">
-            <CardTitle>Available Date Slots</CardTitle>
-            <CardBody>
-              <Container fluid="sm">
-                <Row>
-                {dateSlots}
-                </Row>
-              </Container>
-            </CardBody>
-          </Card>
-          <Card className="p-1">
-            <CardTitle>Available Time Slots</CardTitle>
-            {(visible) ? (
-              <CardBody>
-                <Container>
-                  <Row>
-                    {timeSlots}
-                  </Row>
-                </Container>
-              </CardBody>
-            ):(
-              <CardBody>Select a date</CardBody>
-            )}
-          </Card>
-            <div className="col-12 mt-5">
-              <button style={{backgroundColor:'#f67300', color:'white'}} className="btn btn-block pt-3 pb-3"
-                onClick={postData}
-              >BOOK APPOINTMENTS</button>
-            </div>
-    </Container>
-    </>
+    {
+      (success) ? (
+        <>
+            <nav style={{backgroundColor:'#0079ff'}}>
+            <h5 style={{color: '#ffffff'}} className="pt-3 pb-3 ml-2">Schedule Appointment</h5>
+            </nav>
+            <Container>
+              <p style={{fontWeight:'bold', fontSize:'20px'}}>Schedule Appointment at Merchant Name</p>
+              <Row className="justify-content-around">
+                <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
+                  <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
+                  <p>By Date</p>
+                </Col>
+                <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
+                  <img src="/Clock.png" alt="clock icon" style={{width:'20px', height:'20px' }} />
+                  <p>By Time</p>
+                </Col>
+                <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
+                  <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
+                  <p>By Day</p>
+                </Col>
+                <Col className="d-flex flex-column align-items-center border border-warning rounded p-2 m-2">
+                  <img src="/calendar.png" alt="calender icon" style={{width:'20px', height:'20px' }} />
+                  <p>By Month</p>
+                </Col>
+              </Row>
+              <Card className="p-1">
+                <CardTitle>Available Date Slots</CardTitle>
+                <CardBody>
+                  <Container fluid="sm">
+                    <Row>
+                    {dateSlots}
+                    </Row>
+                  </Container>
+                </CardBody>
+              </Card>
+              <Card className="p-1">
+                <CardTitle>Available Time Slots</CardTitle>
+                {(visible) ? (
+                  <CardBody>
+                    <Container>
+                      <Row>
+                        {timeSlots}
+                      </Row>
+                    </Container>
+                  </CardBody>
+                ):(
+                  <CardBody>Select a date</CardBody>
+                )}
+              </Card>
+                <div className="col-12 mt-5">
+                  <button style={{backgroundColor:'#f67300', color:'white'}} className="btn btn-block pt-3 pb-3"
+                    onClick={postAndSuccess}
+                  >BOOK APPOINTMENTS</button>
+                </div>
+          </Container>
+        </>
+      ) : <Success 
+              serviceName={serviceName}
+              date={selectedDate}
+              time={selectedTime}
+              bpName={bpName}
+          />
+    }
+  </>
   );
 }
 
--- src/SuccessComponent.js
+++ src/SuccessComponent.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Container, Col, Row } from 'reactstrap'
 
-function SuccessComponent() {
+const SuccessComponent = (...props) => {
+    const item = props[0];
+    const dt = new Date(`${item.date}`).toDateString();
+    console.log('selectedSuccesDate', props[0]);
     return (
-        <Container>
-            <Row>
-                
-            </Row>
-            <Row>
-                <p>Appointment Booking Confirmed</p>
-            </Row>
-        </Container>
-    )
+        <div className="d-flex flex-column align-items-center justify-content-center" style={{marginTop:'20%', marginBottom:'20%'}}>
+            <img src="/success.png" style={{height:'70px', width:'70px'}} />
+            <div className="col-auto col-md-8 mt-2 mb-4" style={{fontSize:'20px'}}><b>Appointment Booking Confirmed!</b></div>
+            <div className="col-auto col-md-8 mb-4" style={{fontSize:'18px', fontWeight:'600'}}>Your Appointment for {item.serviceName} at {item.bpName} is booked for {dt} at {item.time}</div>
+            <div className="col-auto col-md-8" style={{fontSize:'15px'}}>Please reach the store 15 mins before the scheduled Appointment</div>
+            <div className="col-12 col-md-4 mt-5">
+                <button style={{backgroundColor:'white', color:'blue'}} className="btn btn-block pt-3 pb-3 border border-primary"
+                >Go Back to <span><img src="/image 1.png" style={{height:'30px', width:'30px'}} /></span></button>
+            </div>
+        </div>
+    )   
 }
 
 export default SuccessComponent
